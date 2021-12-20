601f1d647325f46d957ef39a5c7f93a7b2adfcc3
Danielle Mayabb <dmmayabb@gmail.com>
2021-12-19 21:21:47 -0800

    Commit progress with testing

--- bot/textGenerator.py
+++ bot/textGenerator.py
@@ -1,5 +1,8 @@
 from textgenrnn import textgenrnn 
 
 textgen = textgenrnn()
-textgen.train_from_file('bot/dataset/hallmark-xmas-movies.csv', num_epochs=100)
-textgen.generate(1, temperature=0.5, prefix='', return_as_list=True)
+textgen.train_from_file('./dataset/hallmark-xmas-movies.csv', num_epochs=1)
+# #textgen.generate(1, temperature=0.5, prefix='', return_as_list=True)
+# textgen.generateSamples(100, temperature=0.5, prefix='', return_as_list=True)
+
+textgen.generate(3, temperature=1.0)
