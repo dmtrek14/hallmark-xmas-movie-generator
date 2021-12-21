696913c1bed0bf14ad0fd33f4bb00da365ffaee9
Danielle Mayabb <dmmayabb@gmail.com>
2021-12-20 21:15:04 -0800

    Text generation testing and progress

--- bot/textGenerator.py
+++ bot/textGenerator.py
@@ -1,8 +1,19 @@
 from textgenrnn import textgenrnn 
 
-textgen = textgenrnn()
-textgen.train_from_file('./dataset/hallmark-xmas-movies.csv', num_epochs=1)
-# #textgen.generate(1, temperature=0.5, prefix='', return_as_list=True)
-# textgen.generateSamples(100, temperature=0.5, prefix='', return_as_list=True)
+# textgen = textgenrnn()
+# textgen.train_from_file('./dataset/titles.txt', num_epochs=50, is_csv=False)
 
-textgen.generate(3, temperature=1.0)
+# #generate to file
+# textgen.generate_to_file('./dataset/output-titles.txt', n=250)
+
+titlesBot2 = textgenrnn()
+titlesBot2.train_from_file('./dataset/titles.txt', num_epochs=10, is_csv=False)
+
+#generate to file
+titlesBot2.generate_to_file('./dataset/output-titles-2.txt', n=250)
+
+#summaries
+textgen2 = textgenrnn()
+textgen2.train_from_file('./dataset/summaries.txt', num_epochs=10, is_csv=False)
+
+textgen2.generate_to_file('./dataset/output-summaries.txt', n=250)
